289758868353c1344e9e9819d39ce00f6d6d1c0a
niuhb2 <niuhaibao@gmail.com>
2020-07-01 23:06:37 +0800

    update

--- djcelery/mycelery/config.py
+++ djcelery/mycelery/config.py
@@ -5,5 +5,9 @@
 # 文件名称   ：config.py
 
 
-broker_url = 'redis://192.168.137.131:6379/11'
-result_backend = 'redis://192.168.137.131:6379/12'
+BROKER_URL = 'redis://192.168.137.131:6379/11'
+CELERY_RESULT_BACKEND = 'redis://192.168.137.131:6379/12'
+CELERY_TASK_SERIALIZER = 'msgpack'
+CELERY_RESULT_SERIALIZER = 'json'
+CELERY_TASK_RESULT_EXPIRES = 60 * 60 * 24
+CELERY_ACCEPT_CONTENT = ['json', 'msgpack']
